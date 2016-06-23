9de270e5a7497e35db9aa0ab575d17d4c15c9b08
Phạm Nguyên <Phm Nguyn>
2016-06-23 22:56:00 +0700

    thêm comment

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,8 +5,11 @@ var report = require('./Report');
 
 module.exports = function (app, passport) {
 
+    // quản lý các tag
     tag(app);
+    //quản lý các post
     post(app);
+    //quản l1y các user
     user(app);
 
     //Tất cả request phải đi qua trang index.html để xử lý.
--- server.js
+++ server.js
@@ -2,25 +2,25 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
-
 var bodyParser = require('body-parser');
+var routes =require('./app/routes');
+// db
+var mongoose = require('mongoose');
+var db = require('./config/database');
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var mongoose = require('mongoose');
+
 
 //Kết nối cơ sở dữ liệu
-var db = require('./config/database');
 mongoose.connect(db.url);
 
 app.use(express.static(__dirname + '/public'));
-
-var routes =require('./app/routes');
-
+//route
 routes(app);
-
+//start server
 server.listen(port);
 console.log('Server is running on '+ port);
 exports = module.exports=app;
